40dd93315356cff5871a76ca7f5f8a17f1b81989
yang <89915256@qq.com>
2018-01-19 14:23:59 +0800

    pre-treatment image

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const cv = require('opencv4nodejs');
 
-cv.imreadAsync('./static/img/colors-1.jpg', (err, img) => {
+cv.imreadAsync('./static/img/colors-4.jpg', (err, img) => {
     if (err) {
         console.log(err)
     }
@@ -8,16 +8,24 @@ cv.imreadAsync('./static/img/colors-1.jpg', (err, img) => {
 
     const garyImg = img.bgrToGray()         // 灰度处理
     const blurImg = garyImg.medianBlur(5)   // 中值滤波操作
-    // const blurred = garyImg.blur(new cv.Size(10, 10))
-    // const thresholded = blurred.threshold(
-    //     97,
-    //     143,
-    //     cv.THRESH_BINARY
-    // );
 
-    // detect.detectAsync(garyImg, (err, res) => {
-    //     console.log(res)
-    // })
+    // const blockSize = 9   //计算局部阈值时的每个小窗口(每个局部)的大小,只能为奇数，最小值为3；
+    // const constValue = 0  // 阈值偏移值，函数最终使用的阈值为使用adaptiveMethod计算出的阈值减去C。
+    // const maxVal = 255    // 阈值化后的非零值
+    //
+    // /* 自适应阈值算法
+    // 0：ADAPTIVE_THRESH_MEAN_C
+    // 1: ADAPTIVE_THRESH_GAUSSIAN_C */
+    //
+    // const adaptiveMethod = 0
+    //
+    // /*阈值类型
+    // 0: THRESH_BINARY
+    // 1: THRESH_BINARY_INV */
+    // const thresholdType = 0
+    //
+    // const  thresholdImg = blurImg.adaptiveThreshold(maxVal, adaptiveMethod, thresholdType, blockSize, constValue)
 
-    cv.imwrite('./static/img/colors-change-1.jpg', garyImg)
+    const thresholdImg = blurImg.canny(10, 30, false)
+    cv.imwrite('./static/img/colors-change-1.jpg', thresholdImg)
 })
