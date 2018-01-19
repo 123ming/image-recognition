16880c78d498f32dad7c70307d92a4a312addb99
yang <89915256@qq.com>
2018-01-19 18:18:40 +0800

    how to drawcircle！！！

--- examples/templateMatching.js
+++ examples/templateMatching.js
@@ -1,20 +1,13 @@
-const cv = require('opencv4nodejs');
-// console.log(cv)
+const cv = require('../');
+
 
 const findWaldo = async () => {
   // Load images
-  const originalMat = await cv.imreadAsync(`./static/img/colors-1.jpg`);
-  const waldoMat = await cv.imreadAsync(`./static/img/colors-change-1.jpg`);
+  const originalMat = await cv.imreadAsync(`${__dirname}/templateMatching/original.jpg`);
+  const waldoMat = await cv.imreadAsync(`${__dirname}/templateMatching/waldo.jpg`);
 
   // Match template (the brightest locations indicate the highest match)
   const matched = originalMat.matchTemplate(waldoMat, 5);
-
-  // originalMat.matchTemplateAsync(waldoMat, 5, (err, res) => {
-  //     if (err) {
-  //         console.log('err', err)
-  //     }
-  //     console.log(res)
-  // })
 
   // Use minMaxLoc to locate the highest value (or lower, depending of the type of matching method)
   const minMax = matched.minMaxLoc();
@@ -22,16 +15,15 @@ const findWaldo = async () => {
 
   // Draw bounding rectangle
   originalMat.drawRectangle(
-      new cv.Point(x, y),
-      new cv.Point(x + waldoMat.cols, y + waldoMat.rows),
-      new cv.Vec(0, 255, 0),
-      cv.LINE_8,
-      2
-  )
+    new cv.Rect(x, y, waldoMat.cols, waldoMat.rows),
+    new cv.Vec(0, 255, 0),
+    2,
+    cv.LINE_8
+  );
 
   // Open result in new window
   cv.imshow('We\'ve found Waldo!', originalMat);
-  cv.waitKey(700000);  // time
+  cv.waitKey();
 };
 
 // noinspection JSIgnoredPromiseFromCall
--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const cv = require('opencv4nodejs');
 
-cv.imreadAsync('./static/img/colors-4.jpg', (err, img) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(JSON.stringify(img))
+// cv.imreadAsync('./static/img/white-black-6.jpg', (err, img) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(JSON.stringify(img))
+//
+//     const garyImg = img.bgrToGray()         // 灰度处理
+//     const blurImg = garyImg.medianBlur(5)   // 中值滤波操作
+//
+//     const thresholdImg = blurImg.canny(130, 50, 3, false)
+//     cv.imwrite('./static/img/colors-change-1.jpg', thresholdImg)
+// })
 
-    const garyImg = img.bgrToGray()         // 灰度处理
-    const blurImg = garyImg.medianBlur(5)   // 中值滤波操作
+function imgCut (url) {
+    const initMat = cv.imread(url)
+    const garyMat = initMat.bgrToGray()   // 灰度化
+    const blurMat = garyMat.medianBlur(5)    // 中值滤波操作
+    const thrMat = blurMat.canny(130, 50, 3, false)   // canny边缘检测
+    return thrMat
+}
 
-    // const blockSize = 9   //计算局部阈值时的每个小窗口(每个局部)的大小,只能为奇数，最小值为3；
-    // const constValue = 0  // 阈值偏移值，函数最终使用的阈值为使用adaptiveMethod计算出的阈值减去C。
-    // const maxVal = 255    // 阈值化后的非零值
-    //
-    // /* 自适应阈值算法
-    // 0：ADAPTIVE_THRESH_MEAN_C
-    // 1: ADAPTIVE_THRESH_GAUSSIAN_C */
-    //
-    // const adaptiveMethod = 0
-    //
-    // /*阈值类型
-    // 0: THRESH_BINARY
-    // 1: THRESH_BINARY_INV */
-    // const thresholdType = 0
-    //
-    // const  thresholdImg = blurImg.adaptiveThreshold(maxVal, adaptiveMethod, thresholdType, blockSize, constValue)
+const thrMat = imgCut('./static/init/gary-1.jpg')
 
-    const thresholdImg = blurImg.canny(10, 30, false)
-    cv.imwrite('./static/img/colors-change-1.jpg', thresholdImg)
-})
+// const thresholdImg = blurImg.canny(130, 50, 3, false)
+cv.imwrite('./static/build/new-img.jpg', thrMat)
+
+const detector = new cv.SIFTDetector({ nFeatures: 2000 })
+
+const keyPoints = detector.detect(thrMat)
+
+const descriptors = detector.compute(thrMat, keyPoints)
+
+console.log(descriptors)
