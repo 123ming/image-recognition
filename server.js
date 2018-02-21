d51e87dc057e6aca96e461ea64427c7eea6b0cd5
yang <89915256@qq.com>
2018-02-21 18:49:08 +0800

    sift 检测器参数调整

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const cv = require('opencv4nodejs');
 
-// cv.imreadAsync('./static/img/white-black-6.jpg', (err, img) => {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(JSON.stringify(img))
-//
-//     const garyImg = img.bgrToGray()         // 灰度处理
-//     const blurImg = garyImg.medianBlur(5)   // 中值滤波操作
-//
-//     const thresholdImg = blurImg.canny(130, 50, 3, false)
-//     cv.imwrite('./static/img/colors-change-1.jpg', thresholdImg)
-// })
-
-const url = './static/init/gary-1.jpg'
-
-const initMat = cv.imread(url)
+const url = [
+    './static/init/colors-1.jpg',
+    './static/init/colors-2.jpg',
+    './static/init/colors-3.jpg',
+    './static/init/colors-4.jpg',
+    './static/init/colors-5.jpg',
+    './static/init/colors-6.jpg',
+    './static/init/gary-1.jpg',
+    './static/init/gary-2.jpg',
+    './static/init/gary-3.jpg',
+    './static/init/gary-4.jpg',
+    './static/init/gary-5.jpg',
+    './static/init/gary-6.jpg'
+]
 
 function imgCut (url) {
     const initMat = cv.imread(url)
-    const blurMat = initMat.medianBlur(1)    // 中值滤波操作  // 数值越大图像越模糊
-    const lapMat = blurMat.laplacian(-1, 1)
+    // const blurMat = initMat.medianBlur(1)    // 中值滤波操作  // 数值越大图像越模糊
+    // const lapMat = blurMat.laplacian(-1, 1)
     /*
         拉普拉斯算子锐化
 
@@ -29,7 +27,7 @@ function imgCut (url) {
 
         第二个参数，越大锐化结果越清晰
     */
-    const garyMat = lapMat.bgrToGray()   // 灰度化
+    const garyMat = initMat.bgrToGray()   // 灰度化
     // const eqMat = garyMat.equalizeHist()   // 灰度直方图均衡化 （效果很不好）
 
     // const thresholdMat = garyMat.adaptiveThreshold(35, 1, 1, 3, 0)  //这种阈值分割方法是根据灰度差异
@@ -52,25 +50,36 @@ function imgCut (url) {
         第五个参数，const constValue = 0
         阈值偏移值，函数最终使用的阈值为使用adaptiveMethod计算出的阈值减去C。
     */
-    const thrMat = garyMat.canny(220, 100, 3, false)   // canny边缘检测
+    const thrMat = garyMat.canny(200, 150, 3, false)   // canny边缘检测
 
     return thrMat
 }
 
-const thrMat = imgCut(url)
-
 // const thresholdImg = blurImg.canny(130, 50, 3, false)
 // cv.imwrite('./static/build/new-img.jpg', thrMat)
 
-const detector = new cv.SIFTDetector()  // 构造一个sift检测器
+/*
+    int nfeatures = 0,
+    int nOctaveLayers = 3 ,
+    number contrastThreshold = 0.04 ,
+    number edgeThreshold = 10 ,
+    number sigma = 1.6
+*/
+    const detector = new cv.SIFTDetector(0, 3, 0.04, 50, 1.6)  // 构造一个sift检测器
 
-const keyPoints = detector.detect(thrMat)  // 得到特征点数组集合
+for (let i=0; i<url.length; i++) {
+    const initMat = cv.imread(url[i])
 
-// console.log(keyPoints[0].point)
+    const thrMat = imgCut(url[i])
 
-const img = cv.drawKeyPoints(thrMat, keyPoints)   // 画出特征点
+    const keyPoints = detector.detect(thrMat)  // 得到特征点数组集合
 
-// img.drawCircle(keyPoints[index].point, 15, new cv.Vec(255, 255, 255) , cv.LINE_8 , 1 , 0)
+    // console.log(keyPoints[0].point)
 
-cv.imshow('test', img)
-cv.waitKey(1000000)
+    const img = cv.drawKeyPoints(initMat, keyPoints)   // 画出特征点
+
+    // img.drawCircle(keyPoints[index].point, 15, new cv.Vec(255, 255, 255) , cv.LINE_8 , 1 , 0)
+
+    cv.imshow('t', img)
+    cv.waitKey(1000)
+}
